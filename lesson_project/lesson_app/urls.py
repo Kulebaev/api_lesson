a9deea1701d559be4e791d2148436b61f7845570
Ilya <monah58rus@rambler.ru>
2023-09-21 11:50:39 +0300

    almost end

--- lesson_project/lesson_app/urls.py
+++ lesson_project/lesson_app/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     path('products/', views.ProductList.as_view(), name='product-list'),
     path('lessons/', views.LessonList.as_view(), name='lesson-list'),
     path('product-access/', views.ProductAccessList.as_view(), name='product-access-list'),
-    path('user-lessons/', views.UserLessonListView.as_view(), name='user-lesson-list'),
     path('lesson-views/', views.LessonViewList.as_view(), name='lesson-view-list'),
+
+    # URL для задания
+    path('user-lessons/', views.UserLessonListView.as_view(), name='user-lesson-list'),
     path('api/product-lessons/<int:product_id>/', views.ProductLessonListView.as_view(), name='product-lesson-list'),
     path('api/product-all/', views.ProductStatisticsView.as_view(), name='product-all'),
 ]
